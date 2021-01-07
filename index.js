edaf7bef9e74ccdbf27cffd1acfef169b41f414b
françoise Zimmer <enclasse.fzimmer@gmail.com>
2021-01-07 15:55:07 +0100

    finalisation du dynamisme

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+function afficherNouveauFavori(id)
+{
+	$.getJSON("https://www.googleapis.com/books/v1/volumes/" + id, function(result)
+	{
+		description = typeof result.volumeInfo.description !== 'undefined' ? result.volumeInfo.description.substring(0,201) : "Information manquante";
+		lienImage = typeof result.volumeInfo.imageLinks !== 'undefined' ? (typeof result.volumeInfo.imageLinks.smallThumbnail !== 'undefined' ? result.volumeInfo.imageLinks.smallThumbnail : "./unavailable.png") : "./unavailable.png";
+		$('#content').append(getBookCards(result.volumeInfo.title, result.id, result.volumeInfo.authors[0], description, lienImage, 0));
+	});
+}
 
-
+var booksArray = [];
 $(document).ready(function()
 {
+	var stringifiedArray = sessionStorage.getItem("books");
+	if(stringifiedArray != null)
+	{
+		booksArray = JSON.parse(stringifiedArray);
+		booksArray.forEach(function(id)
+		{
+			afficherNouveauFavori(id);
+		});
+	}
+
 	$(".h3").click(function()
 	{
 		const formulaire = `<div id="form">
 					<input type="text" name="titre du livre" id= "livre"  placeholder="Saisir le titre du livre" required="saisie obligatoire">
 					<input type="text" name="nom de l'auteur" id= "auteur"	placeholder="Saisir le nom de l'auteur.e" required="saisie obligatoire">
 					<input type="button" name="Rechercher" id="go" value="Rechercher">
+					<input type="button" name="Annuler" id="cancel" value="Annuler">
 					</div>`
 		$(".h3").after(formulaire)
+
+		$("#cancel" ).click(function() {
+			$("#form").remove();
+			$('#resultContainer').empty();
+		  });
 
 		$("#go").click(function(){
 			const titleRequest = $("#livre").val();
@@ -17,13 +42,14 @@ $(document).ready(function()
 
 			if(titleRequest == "" || authorRequest == "")
 			{
-				alert ("Au moins un champ est vide");
+				alert ("Au moins un champ est vide !");
 				return;
 			}
 
 			$.getJSON("https://www.googleapis.com/books/v1/volumes?q=intitle:" + titleRequest + "+inauthor:" + authorRequest, function(result)
 			{
 				$('#resultContainer').empty();
+				
 				if(result.totalItems == 0)
 				{
 					alert ("Aucun livre n'a été trouvé !");
@@ -36,14 +62,41 @@ $(document).ready(function()
 					$('#resultContainer').append(getBookCards(item.volumeInfo.title, item.id, item.volumeInfo.authors[0], description, lienImage));
 				});
 			});
+			
 		});
 	});
 });
 
-function getBookCards(_title, _id, _author, _description, _imgLink)
+function getBookCards(_title, _id, _author, _description, _imgLink, type = 1)
 {
-	card = $('<div></div>').addClass('card');
-	card.append($('<i></i>').addClass('fa fa-bookmark'));
+	const icon = $('<i></i>');
+	if(type == 0)
+	{
+		icon.addClass('fa fa-trash');
+		icon.click(function()
+		{
+			booksArray.pop(_id)
+			$('#' + _id + "Fav").remove();
+		});
+	}
+	else if(type == 1)
+	{
+		icon.addClass('fa fa-bookmark');
+		icon.click(function()
+		{
+			if(!booksArray.includes(_id))
+			{
+				booksArray.push(_id);			
+				sessionStorage.setItem("books", JSON.stringify(booksArray));
+				afficherNouveauFavori(_id);
+			}
+			else
+				alert("Vous ne pouvez pas ajouter deux fois le même livre!")
+		})
+	}
+
+	card = $('<div></div>').addClass('card').attr('id', _id + (type == 0 ? "Fav" : "Res"));
+	card.append(icon);
 	card.append($('<h1></h1>').addClass('cardTitle').text("Titre : " + _title));
 	card.append($('<h2></<h2>').addClass('cardId').text("ID : " + _id));
 	card.append($('<h2></<h2>').addClass('cardAuthor').text("Auteur : " + _author));
@@ -51,3 +104,4 @@ function getBookCards(_title, _id, _author, _description, _imgLink)
 	card.append($('<img></<img>').addClass('cardImage').attr({'src' : _imgLink, width : '100%'}).text("Description : " + _description));
 	return card;
 }
+
